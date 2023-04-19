5834872cc2e1ee573927681900323211256786b5
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-04-19 09:09:58 -0300

    change

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ def introduzRuido(dataframe_inicial, num_ruidos):
             lista_ruidos_colocados.append(campos_nao_nulos[n])
             campos_nao_nulos.pop(n)
             dataframe_inicial.loc[cord[0],cord[1]] = randint(0,5)
-    return dataframe_inicial
+    return dataframe_inicial, lista_ruidos_colocados
 
     # lista_random = [randint(0,len(campos_nao_nulos)) for i in range(num_ruidos)]
     # for rd in lista_random:
