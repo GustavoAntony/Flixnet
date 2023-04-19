33e02f7c7c89bb6ab72d4a7d4a05ed6aaff302e0
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-04-19 09:01:28 -0300

    done introduz ruido

--- demo.py
+++ demo.py
@@ -7,18 +7,25 @@ def introduzRuido(dataframe_inicial, num_ruidos):
     # verificando todos os campos que são diferentes de NaN
     nao_nulos = dataframe_inicial.notnull().stack()
     campos_nao_nulos = nao_nulos[nao_nulos].index.tolist()
-    campos_nao_nulos_final = []
-    lista_random = [randint(0,len(campos_nao_nulos)) for i in range(num_ruidos)]
-    novos_valores = np.full_like(dataframe_inicial.values, randint(0,5))
-    for rd in lista_random:
-        campos_nao_nulos_final.append(campos_nao_nulos[rd])
-    # inserindo os novos valores nas coordenadas selecionadas
-    dataframe_inicial.values[campos_nao_nulos_final] = novos_valores[campos_nao_nulos_final]
-
+    for i in range(num_ruidos):
+        if len(campos_nao_nulos) >= 1:
+            n = randint(0,len(campos_nao_nulos)-1)
+            cord = campos_nao_nulos[n]
+            campos_nao_nulos.pop(n)
+            dataframe_inicial.loc[cord[0],cord[1]] = randint(0,5)
     return dataframe_inicial
 
+    # lista_random = [randint(0,len(campos_nao_nulos)) for i in range(num_ruidos)]
+    # for rd in lista_random:
+    #     campos_nao_nulos_final.append(campos_nao_nulos[rd])
+    # for valor in campos_nao_nulos_final:
+    #     dataframe_inicial.loc[valor[0],valor[1]] = randint(0,5)
+    # inserindo os novos valores nas coordenadas selecionadas
 
-df = pd.read_csv("ratings_small.csv")
-matriz = df.pivot_table(index='userId', columns='movieId', values='rating')
 
-print(introduzRuido(matriz, 2))
+data = {'coluna1': ['A', 'A', 'B', 'B'], 'coluna2': ['x', 'y', 'x', 'y'], 'valor': [1, 2, 3, 4]}
+df = pd.DataFrame(data).pivot(index='coluna1', columns='coluna2', values='valor')
+print(df)
+
+df = introduzRuido(df,2)
+print(df)
